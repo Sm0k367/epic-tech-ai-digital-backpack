dd9f83b303f9fca7826b7b2864885b4d4f7162da
Sm0k367 <139561920+Sm0k367@users.noreply.github.com>
2026-03-05 22:17:18 -0500

    Create sitemap.ts

---
+++ apps/host/src/app/sitemap.ts
@@ -0,0 +1,9 @@
+export default function sitemap() {
+  return [
+    { url: '/', lastModified: new Date() },
+    { url: '/music-vault', lastModified: new Date() },
+    { url: '/code-vault', lastModified: new Date() },
+    { url: '/game-labs', lastModified: new Date() },
+    { url: '/chat-nexus', lastModified: new Date() },
+  ];
+}
